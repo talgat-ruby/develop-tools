02283fcc07f50a75f897b4d1beae6623d8eea695
Talgat <talgat_ruby@yahoo.com>
2017-06-12 17:12:38 +0600

    Create bash helper and add to create react directory

---
+++ bash/react/create-directory.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd $PWD/$2
+folder="$1"
+mkdir $folder
+cd $folder
+letter=${folder:0:1}
+folder=${folder/$letter/${letter^^}}
+ind=`expr index "$folder" -`
+while [ "$ind" -gt 0 ] 
+do
+	letter=${folder:$ind:1}
+	folder=${folder/-[a-z]/${letter^^}}
+	ind=`expr index "$folder" -`
+done
+touch $folder.js
+touch $folder.css
+touch $folder.test.js
+touch $folder.flow.js
